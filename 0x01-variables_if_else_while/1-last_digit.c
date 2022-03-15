991d37fde31f9ac446c6cad2016f230210f903e1
blexcel <obichioma123@yahoo.com>
2022-03-15 04:33:02 -0700

    Print the last digit of the number stored in the variable n

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,7 +14,6 @@ int main(void)
 
 	int n;
 	int last_Digit;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	last_Digit = n % 10;
@@ -33,7 +32,6 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_Digit);
 	}
-
 	return (0);
 
 }
