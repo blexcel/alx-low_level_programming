a86388e76c20c7b571ec4aabae9c5f922b362223
blexcel <obichioma123@yahoo.com>
2022-03-16 04:46:40 -0700

    All Base16 numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Printing all base 16 numbers
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int n = '0';
+
+	while (n <= '15')
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+
+}
