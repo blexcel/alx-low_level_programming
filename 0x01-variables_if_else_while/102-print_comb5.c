be7e1e183a4f16c500cd6a9fdafd609fde1e4da8
blexcel <obichioma123@yahoo.com>
2022-03-16 06:47:05 -0700

    Possible two two-digits that meets certain conditions

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Printing combination of two two-digit numbers with certain conditions met
+ * main - Printing combination of two two-digit numbers
  *
  * Return: Always 0 (Success)
  *
