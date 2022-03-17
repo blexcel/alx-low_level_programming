71b9f1ad0b736ed4ca0deca676d0b3114f873561
blexcel <obichioma123@yahoo.com>
2022-03-17 04:36:39 -0700

    Printing _putchar followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Printing _putchar followed by a newline
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
