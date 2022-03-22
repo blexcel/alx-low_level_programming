a300b45b52d541ec4312f55cd49cf9c63d84a5ba
blexcel <obichioma123@yahoo.com>
2022-03-22 03:54:17 -0700

    Prototypes

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,10 +1,11 @@
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
+int _putchar(char c)
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
