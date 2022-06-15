4e1528e0e52805fc0572e0c7f108b9f6a0554237
blexcel <obichioma123@yahoo.com>
2022-06-14 17:13:59 -0700

    Functions and nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - A function that multipies two integers
+ * @a: value for a
+ * @b: value for b
+ *
+ * Return: product
+ */
+
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}
