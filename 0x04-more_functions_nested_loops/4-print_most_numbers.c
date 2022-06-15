94077886496c98ea7f338c3e79343dc2cb21e6d0
blexcel <obichioma123@yahoo.com>
2022-06-15 01:42:16 -0700

    Functions and nested loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,12 +10,12 @@ void print_most_numbers(void)
 {
 	int number;
 
-	for (number = '0'; number <= 9; number++)
+	for (number = '0'; number <= '9'; number++)
 	{
 		if (number != '2' && number != '4')
 		{
 			_putchar(number);
 		}
 	}
-	_putchar('\n')
+	_putchar('\n');
 }
