b1bed2bec438fc4bc77f10be454f5aef7c9a006e
blexcel <obichioma123@yahoo.com>
2022-06-15 04:16:44 -0700

    Function and nested loop

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,7 +12,7 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		putchar(45);
+		_putchar(45);
 		m = n * -1;
 	}
 	else
@@ -26,7 +26,7 @@ void print_number(int n)
 	while (d > 9)
 	{
 		d /= 10;
-		count += 10;
+		count *= 10;
 	}
 
 	for (; count >= 1; count /= 10)
